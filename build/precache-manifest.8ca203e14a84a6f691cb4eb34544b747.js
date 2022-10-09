bd6e1203a6377b0982ac370a5ae8ffe013b0c7e9
Victor Nite <41920643+jumadesigns@users.noreply.github.com>
2022-10-08 23:18:08 -0400

    First initialization

---
+++ build/precache-manifest.8ca203e14a84a6f691cb4eb34544b747.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178760a9e07e793a1921f368462b6551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8303815dce686587a92d",
+    "url": "/static/css/main.c2606501.chunk.css"
+  },
+  {
+    "revision": "4737595f3f2c358573c2",
+    "url": "/static/js/2.96de3fdd.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/static/js/2.96de3fdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8303815dce686587a92d",
+    "url": "/static/js/main.3a6533fb.chunk.js"
+  },
+  {
+    "revision": "a9847047110c8368169f",
+    "url": "/static/js/runtime-main.fac899db.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
